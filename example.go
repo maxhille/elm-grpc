4ff4d0828a1eb2a6670646e7fe7d5626eac55739
Max Hille <mh@lambdasoup.com>
2018-03-23 10:25:19 +0100

    restructure go code

---
+++ example.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"golang.org/x/net/context"
+
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	pb "github.com/maxhille/elm-grpc/build"
+	"google.golang.org/grpc"
+)
+
+type service struct{}
+
+func main() {
+	bindGRPCWEB()
+}
+
+// START GRPC OMIT
+func bindGRPC() {
+	s := grpc.NewServer()
+	pb.RegisterSearchServiceServer(s, service{})
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+// END GRPC OMIT
+
+// START GRPCWEB OMIT
+func bindGRPCWEB() {
+	s := grpc.NewServer()
+	pb.RegisterSearchServiceServer(s, service{})
+
+	// wrap gRPC server into 'normal' go http handlers
+	webGrpc := grpcweb.WrapServer(s)
+	http.HandleFunc("/example.SearchService/", webGrpc.ServeHTTP)
+
+	// serve index.html
+	http.Handle("/", http.FileServer(http.Dir("build")))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// END GRPCWEB OMIT
+
+// START SERVICE OMIT
+func (s service) Search(ctx context.Context, req *pb.SearchRequest) (*pb.SearchResponse, error) {
+	// items: []string{"Mary", "Peter", "Bob", "Alice"}
+	return nil, fmt.Errorf("not implemented yet")
+}
+
+// END SERVICE OMIT
